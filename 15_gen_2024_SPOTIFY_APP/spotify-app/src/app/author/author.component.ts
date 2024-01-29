9fe16704fb7b94ea1ef11acd809e2843e0a7907f
LorenzoVillegas <villegas.lorenzo@iisgalvanimi.edu.it>
2024-01-29 18:58:14 +0000

    album, artist, track (completati)

--- 15_gen_2024_SPOTIFY_APP/spotify-app/src/app/author/author.component.ts
+++ 15_gen_2024_SPOTIFY_APP/spotify-app/src/app/author/author.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SpotifyService } from '../spotify.service';
 
 @Component({
   selector: 'app-author',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./author.component.css']
 })
 export class AuthorComponent {
+  title = 'spotify-app';
+  query: string;
+  obsTrack: Observable<Object>;
+  results: any;
+  // faccio iniettare lo spotify service e faccio una ricerca
+  constructor(public spotify: SpotifyService) {
+   
+  }
 
+  submit(query:HTMLInputElement): void {
+    if (!query.value) {
+      return;
+    }
+    this.query = query.value;
+    this.obsTrack = this.spotify.searchAuthor(this.query);
+    this.obsTrack.subscribe((data) => this.results = data); 
+  }
 }
--- 15_gen_2024_SPOTIFY_APP/spotify-app/src/app/spotify.service.ts
+++ 15_gen_2024_SPOTIFY_APP/spotify-app/src/app/spotify.service.ts
@@ -12,7 +12,31 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
     const headers = new HttpHeaders({
       Authorization:
-        ' Bearer BQDCBS8wvvJIaPegRlKzww3XTzaJVKVS6A28AtQyVqRhZCBgrtE-24wsSy3zZFd98jSzBLMoKbJy3mMmpqJgFmTBP84LbDxm3Xw7jEG4ZnNGUaW_ONUjCY1-reosK6tTuJ1hY87PtlVgNOunmp8P5RVm1tSDAEuZR6ic68yPNbV7V9Sd0JZAxZwoLqYCMYSLg_tbmNwTijVGCl8'
+      'Bearer BQC-wf1A1Pz0pDf_R82zqn_zG1xC8UGF1sQvPjGWnLv8zkWUzybd4JgE83UrkKoKvnNKNl2Ge8tlaIQdVOugS7gkXHrU9YIbXHdLcD6c3sxfj8TQ6d2JGnTfyTBIdz8CUjqREBdpiNep1Ta3eAwIpB71tEH3SG3kr4KBGCvDWBPINQFYbCtthiMX-JgnTBN75srwX7LZVZpq6iQ'
+    });
+
+    let obsTracks = this.http.get(url, { headers });
+    return obsTracks;
+ //Ritorno un observable ai componenti che richiedono il servizio
+  }
+
+  searchAuthor(query: string) {
+    const url = `https://api.spotify.com/v1/search?q=${query}&type=artist`;
+    const headers = new HttpHeaders({
+      Authorization:
+      'Bearer BQC-wf1A1Pz0pDf_R82zqn_zG1xC8UGF1sQvPjGWnLv8zkWUzybd4JgE83UrkKoKvnNKNl2Ge8tlaIQdVOugS7gkXHrU9YIbXHdLcD6c3sxfj8TQ6d2JGnTfyTBIdz8CUjqREBdpiNep1Ta3eAwIpB71tEH3SG3kr4KBGCvDWBPINQFYbCtthiMX-JgnTBN75srwX7LZVZpq6iQ'
+});
+
+    let obsTracks = this.http.get(url, { headers });
+    return obsTracks;
+ //Ritorno un observable ai componenti che richiedono il servizio
+  }
+
+  searchAlbum(query: string) {
+    const url = `https://api.spotify.com/v1/search?q=${query}&type=album`;
+    const headers = new HttpHeaders({
+      Authorization:
+      'Bearer BQC-wf1A1Pz0pDf_R82zqn_zG1xC8UGF1sQvPjGWnLv8zkWUzybd4JgE83UrkKoKvnNKNl2Ge8tlaIQdVOugS7gkXHrU9YIbXHdLcD6c3sxfj8TQ6d2JGnTfyTBIdz8CUjqREBdpiNep1Ta3eAwIpB71tEH3SG3kr4KBGCvDWBPINQFYbCtthiMX-JgnTBN75srwX7LZVZpq6iQ'
     });
 
     let obsTracks = this.http.get(url, { headers });
