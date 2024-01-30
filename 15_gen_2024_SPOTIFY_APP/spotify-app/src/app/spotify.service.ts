f61cf2d46874222a687821fcb2cec67dcdd50c4d
LorenzoVillegas <villegas.lorenzo@iisgalvanimi.edu.it>
2024-01-30 12:58:57 +0000

    spotify service modifica

--- 15_gen_2024_SPOTIFY_APP/spotify-app/src/app/spotify.service.ts
+++ 15_gen_2024_SPOTIFY_APP/spotify-app/src/app/spotify.service.ts
@@ -6,13 +6,19 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class SpotifyService {
   //url per oauth: https://developer.spotify.com/console/get-search-item/
   //Ottengo il modulo HttpClient
-  constructor(private http: HttpClient) { }
 
+  // Dichiarazione della variabile
+  public token: string;
+  
+  constructor(private http: HttpClient) { 
+    //Token da modificare ogni ora
+    this.token = 'Bearer BQCaTQLVjhK0D02Vlu21cuz8JfvGQnIS0l6fPk3JlmqozOj1eomBHlimNvS3Xpej8r1mr7bANFLwO2WLySESkV0PQxz0EOXBHFzNGx6djcsFUNNAPr5yKbcXwge_8rNvbAXk8a7QK2XX_ABpv5DDFDPVbDDtis9h4fWbxScDJnzL3PUWvFHTQweYSD9V7kE_dM_0tCaZxTX8w1g';
+  }
+ 
   searchTrack(query: string) {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
     const headers = new HttpHeaders({
-      Authorization:
-      'Bearer BQC-wf1A1Pz0pDf_R82zqn_zG1xC8UGF1sQvPjGWnLv8zkWUzybd4JgE83UrkKoKvnNKNl2Ge8tlaIQdVOugS7gkXHrU9YIbXHdLcD6c3sxfj8TQ6d2JGnTfyTBIdz8CUjqREBdpiNep1Ta3eAwIpB71tEH3SG3kr4KBGCvDWBPINQFYbCtthiMX-JgnTBN75srwX7LZVZpq6iQ'
+      Authorization: this.token
     });
 
     let obsTracks = this.http.get(url, { headers });
@@ -23,8 +29,7 @@ export class SpotifyService {
   searchAuthor(query: string) {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=artist`;
     const headers = new HttpHeaders({
-      Authorization:
-      'Bearer BQC-wf1A1Pz0pDf_R82zqn_zG1xC8UGF1sQvPjGWnLv8zkWUzybd4JgE83UrkKoKvnNKNl2Ge8tlaIQdVOugS7gkXHrU9YIbXHdLcD6c3sxfj8TQ6d2JGnTfyTBIdz8CUjqREBdpiNep1Ta3eAwIpB71tEH3SG3kr4KBGCvDWBPINQFYbCtthiMX-JgnTBN75srwX7LZVZpq6iQ'
+      Authorization: this.token
 });
 
     let obsTracks = this.http.get(url, { headers });
@@ -35,8 +40,7 @@ export class SpotifyService {
   searchAlbum(query: string) {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=album`;
     const headers = new HttpHeaders({
-      Authorization:
-      'Bearer BQC-wf1A1Pz0pDf_R82zqn_zG1xC8UGF1sQvPjGWnLv8zkWUzybd4JgE83UrkKoKvnNKNl2Ge8tlaIQdVOugS7gkXHrU9YIbXHdLcD6c3sxfj8TQ6d2JGnTfyTBIdz8CUjqREBdpiNep1Ta3eAwIpB71tEH3SG3kr4KBGCvDWBPINQFYbCtthiMX-JgnTBN75srwX7LZVZpq6iQ'
+      Authorization: this.token
     });
 
     let obsTracks = this.http.get(url, { headers });
