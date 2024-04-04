e2308256e7efdba1d14bc5ba45da44a9c8631209
LorenzoVillegas <villegas.lorenzo@iisgalvanimi.edu.it>
2024-04-04 21:06:18 +0000

    little modification

---
+++ OLD_WAY/11_dic_2023_ESERCITAZIONE/caso_2_json/client-angular/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GotyComponent } from './goty/goty.component';
+import { TopComponent } from './top/top.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomepageComponent } from './homepage/homepage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GotyComponent,
+    TopComponent,
+    HomepageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
